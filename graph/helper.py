5824f5b6f7c04c30f2d2f1036326e99fd4235db6
Cedric <cedric@lphn.fr>
2022-11-15 20:44:25 +0100

    feat: helper

--- graph/helper.py
+++ graph/helper.py
@@ -1,26 +1,46 @@
 import os
 import pickle
+import shutil
 from typing import Set, Dict
+import lzma
 
+class Merge:
+    def __init__(self, directory, output):
+        self.directory = directory
+        self.qtable_files = []
+        self.output = output
+        self.qtable = {}
 
-class Helper:
-    def get_qtable_files(directory: str):
-        return {os.path.join(directory, filename) for filename in os.listdir(directory) if filename.endswith('.dat')}
+    def execute(self):
+        self.qtable_files = self.get_qtable_files()
+        self.merge_qtables()
 
-    def merge_qtables(qtable_files: Set[str]):
+    def get_qtable_files(self):
+        return {os.path.join(self.directory, filename) for filename in os.listdir(self.directory) if filename.endswith('.dat')}
+
+    def merge_qtables(self):
         qtable = {}
-        for filename in qtable_files:
-            with open(filename, 'r') as file:
-                current_qtable: Dict[bytes, Dict[str, float]] = pickle.load(file)
+        for filename in self.qtable_files:
+            with lzma.open(filename, 'r') as file:
+                current_qtable = pickle.load(file)
                 for key, value in current_qtable.items():
                     if key not in qtable:
                         qtable[key] = value
                     else:
-                        for action, qvalue in value.items():
-                            if qvalue == 0:
-                                pass
-                            if qtable[key][action] == 0:
-                                qtable[key][action] = qvalue
-                            else:
-                                qtable[key][action] = (qtable[key][action] + qvalue) / 2
-        return qtable
+                        if value == 0:
+                            pass
+                        if qtable[key] == 0:
+                            qtable[key] = value
+                        else:
+                            qtable[key] = (qtable[key] + value) / len(self.qtable_files)
+        self.qtable = qtable
+
+    def save(self):
+        with lzma.open(self.output + ".tmp", 'w') as file:
+            pickle.dump(self.qtable, file)
+        shutil.move(self.output + ".tmp", self.output)
+
+if __name__ == '__main__':
+    test = Merge("../test", "final.dat")
+    test.execute()
+    test.save()
